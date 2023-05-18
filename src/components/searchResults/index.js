26cb63cd59bfa6cb7e4e2eb9fa16bcbf3d92857b
Koci H <husnukoci94@gmail.com>
2023-05-18 11:54:20 +0300

    search result

---
+++ src/components/searchResults/index.js
@@ -0,0 +1,9 @@
+
+
+function SearchResults() {
+    return (
+        <h2> Search Results</h2>
+    )
+}
+
+export default SearchResults
--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,9 +1,12 @@
 import BackButton from "../../components/backbutton"
+import SearchResults from "../../components/searchResults"
 
 function SearchPage () {
     return (
         <>
             <BackButton />
+            <SearchResults />
+            
         </>
     )
 }
