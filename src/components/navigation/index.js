a48846de3cea947e68359d4c4774710d95d32b96
Todd Griffin <todd.griffin61@outlook.com>
2023-05-18 17:45:49 +0100

    created state for user id, which is fetched from the token

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -3,10 +3,14 @@ import CohortIcon from "../../assets/icons/cohortIcon"
 import HomeIcon from "../../assets/icons/homeIcon"
 import ProfileIcon from "../../assets/icons/profileIcon"
 import useAuth from "../../hooks/useAuth"
+import jwt_decode from "jwt-decode"
 import './style.css'
 
 const Navigation = () => {
-	const { token } = useAuth();
+
+  const { token } = useAuth();
+  const { userId } = jwt_decode(token)
+
 
 	if (!token) {
 		return null;
@@ -16,7 +20,7 @@ const Navigation = () => {
         <nav>
             <ul>
                 <li><NavLink to='/'><HomeIcon colour="#000046" /><p>Home</p></NavLink></li>
-                <li><NavLink to='/profile/1'><ProfileIcon /><p>Profile</p></NavLink></li>
+                <li><NavLink to={`/profile/${userId}`}><ProfileIcon /><p>Profile</p></NavLink></li>
                 <li><NavLink to='/MyCohort'><CohortIcon /><p>Cohort</p></NavLink></li>
             </ul>
         </nav>
