329b626845fea45fbf3ce03d7ac155b4c08ef22f
Isabelle <isabel.tartarelli@live.it>
2023-05-18 11:40:42 +0100

    Created branch and added error messages for email and passw on sign up - need to style.

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -6,49 +6,57 @@ import CredentialsCard from "../../components/credentials";
 import "./register.css";
 
 const Register = () => {
-	const { onRegister } = useAuth();
-	const [formData, setFormData] = useState({ email: "", password: "" });
+  const { onRegister } = useAuth();
+  const [formData, setFormData] = useState({ email: "", password: "" });
 
-	const onChange = (e) => {
-		const { name, value } = e.target;
-		setFormData({ ...formData, [name]: value });
-	};
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-	return (
-		<div className="bg-blue register credentialpage">
-			<CredentialsCard
-				title="Register"
-				socialLinksTitle="Or sign up with"
-				altButtonTitle="Already a user?"
-				altButtonLink="/login"
-				altButtonText="Log in"
-			>
-				<div className="register-form">
-					<form>
-						<TextInput
-							value={formData.email}
-							onChange={onChange}
-                            type="email"
-							name="email"
-							label={"Email *"}
-						/>
-						<TextInput
-							value={formData.password}
-							onChange={onChange}
-							name="password"
-							label={"Password *"}
-							type={"password"}
-						/>
-					</form>
-					<Button
-						text="Sign up"
-						onClick={() => onRegister(formData.email, formData.password)}
-						classes="green width-full"
-					/>
-				</div>
-			</CredentialsCard>
-		</div>
-	);
+  return (
+    <div className="bg-blue register credentialpage">
+      <CredentialsCard
+        title="Register"
+        socialLinksTitle="Or sign up with"
+        altButtonTitle="Already a user?"
+        altButtonLink="/login"
+        altButtonText="Log in"
+      >
+        <div className="register-form">
+          <form>
+            <TextInput
+              value={formData.email}
+              onChange={onChange}
+              type="email"
+              name="email"
+              label={"Email *"}
+            />
+            <p className="email-error-message">
+              {" "}
+              * Email address must be in a valid format.
+            </p>
+            <TextInput
+              value={formData.password}
+              onChange={onChange}
+              name="password"
+              label={"Password *"}
+              type={"password"}
+            />
+            <p className="passw-error-message">
+              * Password must contain at least one uppercase letter, one number,
+              one special character and be at least 8 characters long.
+            </p>
+          </form>
+          <Button
+            text="Sign up"
+            onClick={() => onRegister(formData.email, formData.password)}
+            classes="green width-full"
+          />
+        </div>
+      </CredentialsCard>
+    </div>
+  );
 };
 
 export default Register;
