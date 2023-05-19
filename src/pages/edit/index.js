b5fe3b6fe51e52ca849415f2394c20cdc7ce9787
davidbrockway <dav.brock@protonmail.com>
2023-05-19 11:02:28 +0100

    Updated <hr> tag to CSS

--- src/pages/edit/index.js
+++ src/pages/edit/index.js
@@ -5,11 +5,6 @@ import "./style.css";
 import "../../styles/index.css";
 import LockIcon from "../../assets/icons/locIcon";
 
-const hrStyle = {
-  backgroundColor: "e6ebf5",
-  height: 1,
-};
-
 const EditProfile = () => {
   return (
     <>
@@ -28,7 +23,7 @@ const EditProfile = () => {
 
           <div className="main-info-grid">
             <div className="basic-info">
-              <hr style={hrStyle} />
+           <hr class="hr-line" />
               <br />
               <div className="basic-info-content info-grid">
                 <h4 className="padding-title">Basic info</h4>
@@ -75,7 +70,7 @@ const EditProfile = () => {
             </div>
 
             <div className="training-info">
-              <hr style={hrStyle} />
+           <hr class="hr-line" />
               <br />
               <h4 className="padding-title">Training info</h4>
               <div className="training-info-content info-grid">
@@ -152,7 +147,7 @@ const EditProfile = () => {
             </div>
 
             <div className="contact-info">
-              <hr style={hrStyle} />
+           <hr class="hr-line" />
               <div className="contact-info-content info-grid">
                 <h4 className="padding-title">Contact info</h4>
                 <div className="padding-top"></div>
@@ -209,7 +204,7 @@ const EditProfile = () => {
 
             <div className="bio">
               <div className="bio-content">
-                <hr style={hrStyle} />
+             <hr class="hr-line" />
                 <h4>Bio</h4>
                 <div className="padding-top"></div>
                 <small>Bio</small>
