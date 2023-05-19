803a5a7b3d9d8b58f7a151af11675029050c19a2
Isabelle <isabel.tartarelli@live.it>
2023-05-19 11:29:13 +0100

    Created new branch for new task and imported components from old branch

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -3,6 +3,7 @@ import Stepper from "../../components/stepper";
 import useAuth from "../../hooks/useAuth";
 import StepOne from "./stepOne";
 import StepTwo from "./stepTwo";
+import StepFour from "./stepFour";
 import "./style.css";
 
 const Welcome = () => {
@@ -43,6 +44,7 @@ const Welcome = () => {
       <Stepper header={<WelcomeHeader />} onComplete={onComplete}>
         <StepOne data={profile} setData={onChange} />
         <StepTwo data={profile} setData={onChange} />
+        <StepFour data={profile} setData={onChange} />
       </Stepper>
     </main>
   );
