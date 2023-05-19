e4a5855e8fd254af3425a6587cb1a3da8738e131
Isabelle <isabel.tartarelli@live.it>
2023-05-19 10:42:48 +0100

    Created stepFour component and moved stepTwo code to the new one

---
+++ src/pages/welcome/stepFour/index.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import Stepper from "../../../components/stepper";
+import useAuth from "../../../hooks/useAuth";
+import StepOne from "../stepOne";
+import StepTwo from "./stepTwo";
+import "./style.css";
+
+const Welcome = () => {
+  const { onCreateProfile } = useAuth();
+
+  const [profile, setProfile] = useState({
+    firstName: "",
+    lastName: "",
+    githubUsername: "",
+    bio: "",
+  });
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+
+    setProfile({
+      ...profile,
+      [name]: value,
+    });
+  };
+
+  const onComplete = () => {
+    onCreateProfile(
+      profile.firstName,
+      profile.lastName,
+      profile.githubUsername,
+      profile.bio
+    );
+  };
+
+  return (
+    <main className="welcome">
+      <div className="welcome-titleblock">
+        <h1 className="h2">Welcome to Cohort Manager</h1>
+        <p className="text-blue1">Create your profile to get started</p>
+      </div>
+
+      <Stepper header={<WelcomeHeader />} onComplete={onComplete}>
+        <StepOne data={profile} setData={onChange} />
+        <StepTwo data={profile} setData={onChange} />
+      </Stepper>
+    </main>
+  );
+};
+
+const WelcomeHeader = () => {
+  return (
+    <div className="welcome-cardheader">
+      <h2>Create Profile</h2>
+      <p className="text-blue1">
+        Tell us about yourself to create your profile
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
