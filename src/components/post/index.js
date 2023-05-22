b098e18d2cd8a233e07f55ffcbc065298263da7d
carolina arruda <carolinacalouroarruda@gmail.com>
2023-05-22 16:37:31 +0200

    working like button

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,88 +1,104 @@
-import Like from "../../assets/icons/like"
-import LikeRed from "../../assets/icons/likeRed"
-import useModal from "../../hooks/useModal"
-import Card from "../card"
-import Comment from "../comment"
-import EditPostModal from "../editPostModal"
-import ProfileCircle from "../profileCircle"
-import "./style.css"
-import {useState} from 'react' 
+import React, { useState } from "react";
+import Like from "../../assets/icons/like";
+import LikeRed from "../../assets/icons/likeRed";
+import useModal from "../../hooks/useModal";
+import Card from "../card";
+import Comment from "../comment";
+import EditPostModal from "../editPostModal";
+import ProfileCircle from "../profileCircle";
+import "./style.css";
 
-const Post = ({ name, date, content, comments = [], likes = 0, id, setTriggerUpdate, currentUserName}) => {
-    const { openModal, setModal } = useModal()
+const Post = ({
+  name,
+  date,
+  content,
+  comments = [],
+  likes = 0,
+  id,
+  setTriggerUpdate,
+  currentUserName,
+}) => {
+  const { openModal, setModal } = useModal();
 
-    const userInitials = name.match(/\b(\w)/g)
+  const userInitials = name.match(/\b(\w)/g);
+  const [isLiked, setIsLiked] = useState(false);
+  const [like, setLike] = useState(likes);
 
-    const [isHovered, setIsHovered] = useState(false);
-    const [changeLike, setChangeLike] = useState(false);
+  const showModal = () => {
+    setModal(
+      "Edit post",
+      <EditPostModal
+        id={id}
+        content={content}
+        setTriggerUpdate={setTriggerUpdate}
+      />
+    );
+    openModal();
+  };
 
-    const handleMouseEnter = () => {
-      setIsHovered(true);
-    };
-  
-    const handleMouseLeave = () => {
-      setIsHovered(false);
-    };
+  const handleClick = () => {
+    setIsLiked((prevIsLiked) => !prevIsLiked);
+    setLike((prevLike) => (isLiked ? prevLike - 1 : prevLike + 1));
+  };
 
-    const handleClick = () => {
-        setChangeLike(true)
-    }
+  let likeIcon = null;
+  if (isLiked) {
+    likeIcon = <LikeRed />;
+  } else {
+    likeIcon = <Like />;
+  }
 
-    const showModal = () => {
-        setModal('Edit post', 
-        <EditPostModal 
-            id={id} 
-            content={content} 
-            setTriggerUpdate={setTriggerUpdate}
-        />)
-        openModal()
-    }
+  return (
+    <Card>
+      <article className="post">
+        <section className="post-details">
+          <ProfileCircle initials={userInitials} />
 
-    return (
-        <Card>
-            <article className="post">
-                <section className="post-details">
-                    <ProfileCircle initials={userInitials} />
+          <div className="post-user-name">
+            <p>{name}</p>
+            <small>{date}</small>
+          </div>
+          {name === currentUserName && (
+            <div className="edit-icon">
+              <p onClick={showModal}>...</p>
+            </div>
+          )}
+        </section>
 
-                    <div className="post-user-name">
-                        <p>{name}</p>
-                        <small>{date}</small>
-                    </div>
-                    {name === currentUserName &&
-                        <div className="edit-icon">
-                            <p onClick={showModal}>...</p>
-                        </div>
-                    }
-                </section>
+        <section className="post-content">
+          <p>{content}</p>
+        </section>
 
-                <section className="post-content">
-                    <p>{content}</p>
-                </section>
+        <section
+          className={`post-interactions-container border-top ${
+            comments.length ? "border-bottom" : null
+          }`}
+        >
+          <button className="post-interactions-button like" onClick={handleClick}>
+            {likeIcon}
+            <p>Like</p>
+          </button>
 
-                <section className={`post-interactions-container border-top ${comments.length ? 'border-bottom' : null}`}>
-                <button className="post-interactions-button like"
-                 onMouseEnter={handleMouseEnter}
-                 onMouseLeave={handleMouseLeave}
-                 onClick={handleClick}>
-                {isHovered ? <LikeRed /> : <Like />}
-                            <p>Like</p>
-                </button>
+          <button className="post-interactions-button">
+            <Like />
+            <p>Like</p>
+          </button>
 
-                <button className="post-interactions-button">
-                            <Like />
-                            <p>Like</p>
-                </button>
+          <p>{!like && "Be the first to like this"}</p>
+        </section>
 
-                    <p>{!likes && 'Be the first to like this'}</p>
-                    
-                </section>
+        <section>
+          {comments.map((comment) => (
+            <Comment
+              key={comment.id}
+              name={comment.name}
+              content={comment.content}
+            />
+          ))}
+        </section>
+      </article>
+    </Card>
+  );
+};
 
-                <section>
-                    {comments.map(comment => <Comment key={comment.id} name={comment.name} content={comment.content} />)}
-                </section>
-            </article>
-        </Card>
-    )
-}
-
-export default Post
+export default Post;
