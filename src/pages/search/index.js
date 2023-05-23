faadb9aa978a47d05a52f48d98f81cff832ef7bf
Lee Kelly <leeowenkelly@gmail.com>
2023-05-23 12:13:54 +0100

    duplicate code removed

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -82,134 +82,77 @@ function SearchPage () {
         }
 
     return (
-        <>
-            {userRole === 'TEACHER' ? (
-                    <div className="parent">
-                    <BackButton />
-                    <h1>Search Results</h1>
-                    <section className="searchparent">
-                        <div className="">
-                            {formData.length !== 0 
-                            ? (<form onSubmit={onSubmit}>
+        <>  
+            <div className="parent">
+                <BackButton />
+                <h1>Search Results</h1>
+                <section className="searchparent">
+                    <div className="">
+                        {formData.length !== 0 
+                        ? (<form onSubmit={onSubmit}>
+                        <TextInput
+                        value={formData}
+                        onChange={onChange}
+                        name={'searchusers'}
+                        type="text"
+                        icon={<SearchIcon />}/>
+
+                        <button className="clearbutton">
+                            <CrossBlackIcon/>
+                        </button>
+                        
+                        </form>) : (
+                            <form onSubmit={onSubmit}>
                             <TextInput
                             value={formData}
                             onChange={onChange}
                             name={'searchusers'}
                             type="text"
                             icon={<SearchIcon />}/>
-
-                            <button className="clearbutton">
-                                <CrossBlackIcon/>
-                            </button>
                             
-                            </form>) : (
-                                <form onSubmit={onSubmit}>
-                                <TextInput
-                                value={formData}
-                                onChange={onChange}
-                                name={'searchusers'}
-                                type="text"
-                                icon={<SearchIcon />}/>
-                                </form>
-                            )}
+                            </form>
+                        )}
 
-                        </div>
-                    </section>
-
-                    <section className="resultsparent">
-                        <div className="resultslist">
-                        <p>People</p>
-                            <ul>
-                                {results.map((obj, index) => {
-                                    return (
-                                        <li key={index} >
-                                            <div className="profile-icon search-picture">
-                                                <p>{obj.firstName[0]}{obj.lastName[0]}</p>
-                                            </div>
-                                            <div>
-                                                <h4>{obj.firstName} {obj.lastName}</h4>
-                                                {obj.cohortId !== null
-                                                    ? <p className="extrainfo">{obj.role}, Cohort {obj.cohortId}</p>
-                                                    : <p className="extrainfo">{obj.role}</p>
-                                                }
-                                            </div>
-                                            <Link to={`/profile/${obj.id}`} className="profiles">Profile</Link>
-                                            <div>Add Note</div>
-                                            <div>Move to Cohort</div>
-                                            <button id="search-more" onClick={() => clickShowMore(index)} >...</button>
-                                            {showMore === index && (<div className="showmore" ref={ref}><Link to={`/profile/${obj.id}`} className="dropprofiles"><div className="dropicon"><ProfileIcon /></div><div className="droptext">Profile</div></Link></div>)}
-                                        </li>
-                                    )         
-                                })}                            
-                            </ul>
-                        </div>
-                    </section>
-                </div>
-            ) :
-                (
-                    <div className="parent">
-                        <BackButton />
-                        <h1>Search Results</h1>
-                        <section className="searchparent">
-                            <div className="">
-                                {formData.length !== 0 
-                                ? (<form onSubmit={onSubmit}>
-                                <TextInput
-                                value={formData}
-                                onChange={onChange}
-                                name={'searchusers'}
-                                type="text"
-                                icon={<SearchIcon />}/>
-
-                                <button className="clearbutton">
-                                    <CrossBlackIcon/>
-                                </button>
-                                
-                                </form>) : (
-                                    <form onSubmit={onSubmit}>
-                                    <TextInput
-                                    value={formData}
-                                    onChange={onChange}
-                                    name={'searchusers'}
-                                    type="text"
-                                    icon={<SearchIcon />}/>
-                                    
-                                    </form>
-                                )}
-
-                            </div>
-                        </section>
-
-                        <section className="resultsparent">
-                            <div className="resultslist">
-                            <p>People</p>
-                                <ul>
-                                    {results.map((obj, index) => {
-                                        return (
-                                            <li key={index} >
-                                                <div className="profile-icon search-picture">
-                                                    <p>{obj.firstName[0]}{obj.lastName[0]}</p>
-                                                </div>
-                                                <div>
-                                                    <h4>{obj.firstName} {obj.lastName}</h4>
-
-                                                    {obj.cohortId !== null
-                                                        ? <p className="extrainfo">{obj.role}, Cohort {obj.cohortId}</p>
-                                                        : <p className="extrainfo">{obj.role}</p>
-                                                    }
-                                                </div>
-                                                <Link to={`/profile/${obj.id}`} className="profiles">Profile</Link>
-                                                <button id="search-more" onClick={() => clickShowMore(index)} >...</button>
-                                                {showMore === index && (<div className="showmore" ref={ref}><Link to={`/profile/${obj.id}`} className="dropprofiles"><div className="dropicon"><ProfileIcon /></div><div className="droptext">Profile</div></Link></div>)}
-                                            </li>
-                                        )         
-                                    })}                            
-                                </ul>
-                            </div>
-                        </section>
                     </div>
-                )    
-            }       
+                </section>
+
+                <section className="resultsparent">
+                    <div className="resultslist">
+                    <p>People</p>
+                        <ul>
+                            {results.map((obj, index) => {
+                                return (
+                                    <li key={index} >
+                                        <div className="profile-icon search-picture">
+                                            <p>{obj.firstName[0]}{obj.lastName[0]}</p>
+                                        </div>
+                                        <div>
+                                            <h4>{obj.firstName} {obj.lastName}</h4>
+
+                                            {obj.cohortId !== null
+                                                ? <p className="extrainfo">{obj.role}, Cohort {obj.cohortId}</p>
+                                                : <p className="extrainfo">{obj.role}</p>
+                                            }
+                                        </div>
+                                        <Link to={`/profile/${obj.id}`} className="profiles">Profile</Link>
+                                        { 
+                                            userRole === 'TEACHER' && (
+                                                <>
+                                                    <div>Add Note</div>
+                                                    <div>Move to Cohort</div>
+                                                </>
+                                            )
+                                        }
+                                        <button id="search-more" onClick={() => clickShowMore(index)} >...</button>
+                                        {showMore === index && (<div className="showmore" ref={ref}><Link to={`/profile/${obj.id}`} className="dropprofiles"><div className="dropicon"><ProfileIcon /></div><div className="droptext">Profile</div></Link></div>)}
+                                    </li>
+                                )         
+                            })}                            
+                        </ul>
+                    </div>
+                </section>
+            </div>
+
         </>
     )
 }
