8e6860c57d47b9800f25c3987b3caef3cc6ad645
Isabelle <isabel.tartarelli@live.it>
2023-05-23 17:39:07 +0100

    Deleted extra return

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -34,23 +34,7 @@ const Posts = ({triggerUpdate, setTriggerUpdate, currentUser}) => {
     )
 }
 
-  return (
-    <>
-      {posts.map((post) => {
-        return (
-          <Post
-            key={post.id}
-            name={`${post.author.firstName} ${post.author.lastName}`}
-            date={post.createdAt}
-            content={post.content}
-            comments={post.comments}
-            id={post.id}
-            setTriggerUpdate={setTriggerUpdate}
-          />
-        );
-      })}
-    </>
-  );
-};
+  
+
 
 export default Posts;
