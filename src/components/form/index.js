11afdcfb444de412223abe1a8bac1920564a5568
Isabelle <isabel.tartarelli@live.it>
2023-05-24 17:16:40 +0100

    Formatted files

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,14 +1,19 @@
-const Form = ({ children, onSubmit, className,  onMouseDown, onFocus }) => {
-    const onFormSubmit = (event) => {
-        event.preventDefault()
-        onSubmit()
-    }
+const Form = ({ children, onSubmit, className, onMouseDown, onFocus }) => {
+	const onFormSubmit = (event) => {
+		event.preventDefault()
+		onSubmit()
+	}
 
-    return (
-        <form className={className} onSubmit={onFormSubmit} onMouseDown={onMouseDown} onFocus={onFocus}>
-            {children}
-        </form>
-    )
+	return (
+		<form
+			className={className}
+			onSubmit={onFormSubmit}
+			onMouseDown={onMouseDown}
+			onFocus={onFocus}
+		>
+			{children}
+		</form>
+	)
 }
 
-export default Form;
+export default Form
