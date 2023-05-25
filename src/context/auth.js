405eb02f65036e89a1559ddb5e59ce518ac3c4c3
Ian Grantham <dev.iangrantham@gmail.com>
2023-05-25 17:50:17 +0100

    updated useEffect dependencies

--- src/context/auth.js
+++ src/context/auth.js
@@ -37,7 +37,7 @@ const AuthProvider = ({ children }) => {
       setToken(storedToken);
 
     }
-  }, [navigate, token]);
+  }, [navigate, token, profile]);
 
   const handleLogin = async (email, password, successRoute = "/") => {
     const res = await login(email, password);
