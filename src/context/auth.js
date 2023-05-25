903c7b15072e584bf2a6e4ba1707b75bdf0d490d
Ian Grantham <dev.iangrantham@gmail.com>
2023-05-25 15:55:06 +0100

    fixing errors in create profile being able to leave page

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,97 +1,112 @@
-import { createContext, useEffect, useState } from 'react'
-import { useNavigate, useLocation, Navigate } from 'react-router-dom'
-import Header from '../components/header'
-import Modal from '../components/modal'
-import Navigation from '../components/navigation'
-import useAuth from '../hooks/useAuth'
-import { createProfile, login, register } from '../service/apiClient'
-import jwt_decode from 'jwt-decode'
+import { createContext, useEffect, useState } from "react";
+import { useNavigate, useLocation, Navigate } from "react-router-dom";
+import Header from "../components/header";
+import Modal from "../components/modal";
+import Navigation from "../components/navigation";
+import useAuth from "../hooks/useAuth";
+import {
+  createProfile,
+  getUserProfile,
+  login,
+  register,
+} from "../service/apiClient";
+import jwt_decode from "jwt-decode";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-	const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [profile, setProfile] = useState(null);
 
-	const [token, setToken] = useState(null)
-	const [userId, setUserId] = useState(null)
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
 
-	useEffect(() => {
-		const storedToken = localStorage.getItem('token')
+    if (storedToken && !token) {
+      setToken(storedToken);
+      navigate("/");
+    }
+    if (token) {
+      const decoded = jwt_decode(token);
+      setUserId(decoded.userId);
+      getUserProfile(decoded.userId).then((profile) => {
+        setProfile(profile);
+      });
+    }
+  }, [navigate, token]);
 
-		if (storedToken && !token) {
-			setToken(storedToken)
-			navigate('/')
-		}
-		if (token) {
-			const decoded = jwt_decode(token)
-			setUserId(decoded.userId)
-		}
-	}, [navigate, token])
+  const handleLogin = async (email, password, successRoute = "/") => {
+    const res = await login(email, password);
 
-	const handleLogin = async (email, password, successRoute = '/') => {
-		const res = await login(email, password)
+    if (!res.data.token) {
+      return navigate("/login");
+    }
 
-		if (!res.data.token) {
-			return navigate('/login')
-		}
+    localStorage.setItem("token", res.data.token);
 
-		localStorage.setItem('token', res.data.token)
+    setToken(res.data.token);
+    navigate(successRoute);
+  };
 
-		setToken(res.data.token)
-		navigate(successRoute)
-	}
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+  };
 
-	const handleLogout = () => {
-		localStorage.removeItem('token')
-		setToken(null)
-	}
+  const handleRegister = async (email, password) => {
+    const res = await register(email, password);
+    if (res.status === "fail") {
+      throw new Error(res.data.error);
+    }
+    setToken(res.data.token);
+    navigate("/verification");
+  };
 
-	const handleRegister = async (email, password) => {
-		const res = await register(email, password)
-		if (res.status === 'fail') {
-			throw new Error(res.data.error)
-		}
-		setToken(res.data.token)
-		navigate('/verification')
-	}
+  const handleCreateProfile = async (firstName, lastName, githubUrl, bio) => {
+    const { userId } = jwt_decode(token);
 
-	const handleCreateProfile = async (firstName, lastName, githubUrl, bio) => {
-		const { userId } = jwt_decode(token)
+    const user = await createProfile(userId, firstName, lastName, githubUrl, bio);
+	setProfile(user.data.profile)
+	console.log('temp',user)
+    localStorage.setItem("token", token);
+    navigate("/");
+  }; 
 
-		await createProfile(userId, firstName, lastName, githubUrl, bio)
+  const value = {
+    token,
+    userId,
+    profile,
+    onLogin: handleLogin,
+    onLogout: handleLogout,
+    onRegister: handleRegister,
+    onCreateProfile: handleCreateProfile,
+  };
 
-		localStorage.setItem('token', token)
-		navigate('/')
-	}
-
-	const value = {
-		token,
-		userId,
-		onLogin: handleLogin,
-		onLogout: handleLogout,
-		onRegister: handleRegister,
-		onCreateProfile: handleCreateProfile,
-	}
-
-	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
-}
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
 
 const ProtectedRoute = ({ children }) => {
-	const { token } = useAuth()
-	const location = useLocation()
+  const { token, profile } = useAuth();
+  const location = useLocation();
 
-	if (!token) {
-		return <Navigate to={'/login'} replace state={{ from: location }} />
-	}
+  if (!token) {
+    return <Navigate to={"/login"} replace state={{ from: location }} />;
+  }
+  console.log('location path name', location.pathname)
+  console.log('profile', profile)
+  if ((!profile || !profile.firstName) && location.pathname !== '/welcome') {
+    return <Navigate to={"/welcome"} replace state={{ from: location }} />;
+  }
 
-	return (
-		<div className="container">
-			<Header />
-			<Navigation />
-			<Modal />
-			{children}
-		</div>
-	)
-}
+  return (
+    <div className="container">
+      <Header />
+      <Navigation />
+      <Modal />
+      {children}
+    </div>
+  );
+};
 
-export { AuthContext, AuthProvider, ProtectedRoute }
+export { AuthContext, AuthProvider, ProtectedRoute };
